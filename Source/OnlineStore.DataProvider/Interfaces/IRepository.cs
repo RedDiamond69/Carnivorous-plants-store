b778cef4566664025b6277f88d662024fb8d682f
Alexey Kosyuk <reddiamond821@gmail.com>
2019-04-09 13:29:33 +0300

    Add IRepository interface.

---
+++ Source/OnlineStore.DataProvider/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.DataProvider.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetByID(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(T item);
+    }
+}
