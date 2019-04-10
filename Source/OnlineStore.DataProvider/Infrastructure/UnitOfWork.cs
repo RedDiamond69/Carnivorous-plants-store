7bba2a22f4ed4300c67ea5b6a2657f25b0b4cc88
Alexey Kosyuk <reddiamond821@gmail.com>
2019-04-10 04:01:48 +0300

    Add IUnitOfWork interface and UnitOfWork class.

---
+++ Source/OnlineStore.DataProvider/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using OnlineStore.DataProvider.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.DataProvider
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
