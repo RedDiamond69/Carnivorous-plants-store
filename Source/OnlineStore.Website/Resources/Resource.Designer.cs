9441597a5239564802dd869a2d028cc917dbd516
Alexey Kosyuk <reddiamond821@gmail.com>
2019-05-14 02:57:26 +0300

    Add layout page static text.

--- Source/OnlineStore.Website/Resources/Resource.Designer.cs
+++ Source/OnlineStore.Website/Resources/Resource.Designer.cs
@@ -61,11 +61,209 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Уход и Содержание.
+        /// </summary>
+        public static string CareAndMaintaince {
+            get {
+                return ResourceManager.GetString("CareAndMaintaince", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Корзина пуста..
+        /// </summary>
+        public static string CartEmpty {
+            get {
+                return ResourceManager.GetString("CartEmpty", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Каталог.
+        /// </summary>
+        public static string Catalog {
+            get {
+                return ResourceManager.GetString("Catalog", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Цефалотус.
+        /// </summary>
+        public static string Cephalotus {
+            get {
+                return ResourceManager.GetString("Cephalotus", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Контакты.
+        /// </summary>
+        public static string Contacts {
+            get {
+                return ResourceManager.GetString("Contacts", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Контакты:.
+        /// </summary>
+        public static string ContactUs {
+            get {
+                return ResourceManager.GetString("ContactUs", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Дарлингтония Калифорнийская.
+        /// </summary>
+        public static string DarlingtoniaCalifornica {
+            get {
+                return ResourceManager.GetString("DarlingtoniaCalifornica", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Венерина Мухоловка.
+        /// </summary>
+        public static string DionaeaMuscipula {
+            get {
+                return ResourceManager.GetString("DionaeaMuscipula", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Росянка.
+        /// </summary>
+        public static string Drosera {
+            get {
+                return ResourceManager.GetString("Drosera", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Следите за нами в социальных сетях:.
+        /// </summary>
+        public static string FollowUs {
+            get {
+                return ResourceManager.GetString("FollowUs", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Галерея.
+        /// </summary>
+        public static string Gallery {
+            get {
+                return ResourceManager.GetString("Gallery", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Главная.
+        /// </summary>
+        public static string Home {
+            get {
+                return ResourceManager.GetString("Home", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Venusflytrap.com - онлайн-магазин хищных растений в Минске!.
         /// </summary>
         public static string HomeTitle {
             get {
                 return ResourceManager.GetString("HomeTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Язык.
+        /// </summary>
+        public static string Language {
+            get {
+                return ResourceManager.GetString("Language", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Непентес.
+        /// </summary>
+        public static string Nepenthes {
+            get {
+                return ResourceManager.GetString("Nepenthes", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Venusflytrap.com - &lt;strong&gt;первый интернет - магазин хищных растений &lt;/strong&gt; в Минске! В нашем магазине вы можете приобрести &lt;strong&gt;более &lt;span class=&quot;text-danger&quot;&gt;200&lt;/span&gt; редких сортов хищных растений со всего мира&lt;/strong&gt;, а также абсолютно всё, что требуется для выращивания: специализированные субстраты, компоненты субстрата, горшки, дистиллированная вода, препараты и многое другое!.
+        /// </summary>
+        public static string OnlineStoreFooterAbout {
+            get {
+                return ResourceManager.GetString("OnlineStoreFooterAbout", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Оплата и Доставка.
+        /// </summary>
+        public static string PaymentAndDelivery {
+            get {
+                return ResourceManager.GetString("PaymentAndDelivery", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Жирянка.
+        /// </summary>
+        public static string Pinguicula {
+            get {
+                return ResourceManager.GetString("Pinguicula", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Саррацения.
+        /// </summary>
+        public static string Sarracenia {
+            get {
+                return ResourceManager.GetString("Sarracenia", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Найти.
+        /// </summary>
+        public static string Search {
+            get {
+                return ResourceManager.GetString("Search", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Войти.
+        /// </summary>
+        public static string SingIn {
+            get {
+                return ResourceManager.GetString("SingIn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Акции.
+        /// </summary>
+        public static string Stock {
+            get {
+                return ResourceManager.GetString("Stock", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Пузырчатка.
+        /// </summary>
+        public static string Utricularia {
+            get {
+                return ResourceManager.GetString("Utricularia", resourceCulture);
             }
         }
     }
