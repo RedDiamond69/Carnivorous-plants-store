8f249466ff8f2c7049b8c1db594b9a3c8da2bc8b
Alexey Kosyuk <reddiamond821@gmail.com>
2019-05-16 17:33:43 +0300

    Add ILanguageService, LanguageService and LanguageDTO.

---
+++ Source/OnlineStore.Logic/Services/LanguageService.cs
@@ -0,0 +1,59 @@
+﻿using OnlineStore.Logic.Interfaces;
+using OnlineStore.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Logic.Services
+{
+    public class LanguageService : ILanguageService
+    {
+        public void Add(LanguageDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<LanguageDTO> models)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<LanguageDTO> Find(Expression<Func<LanguageDTO, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LanguageDTO Get(string guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<LanguageDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(LanguageDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<LanguageDTO> models)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(LanguageDTO model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
