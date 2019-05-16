5faac83c15fd234e2dd39ac3fdf9704912ea4e0c
Alexey Kosyuk <reddiamond821@gmail.com>
2019-05-16 15:58:27 +0300

    Add IProductService and ProductService.

---
+++ Source/OnlineStore.Logic/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using OnlineStore.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Logic.Interfaces
+{
+    public interface IProductService : IService<ProductDTO>
+    {
+    }
+}
---
+++ Source/OnlineStore.Logic/Services/ProductService.cs
@@ -0,0 +1,59 @@
+﻿using OnlineStore.Logic.Interfaces;
+using OnlineStore.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Logic.Services
+{
+    public class ProductService : IProductService
+    {
+        public void Add(ProductDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<ProductDTO> models)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProductDTO> Find(Expression<Func<ProductDTO, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductDTO Get(string guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProductDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(ProductDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<ProductDTO> models)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ProductDTO model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
