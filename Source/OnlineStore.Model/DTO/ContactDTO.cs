cc5816684cae90f8240cedaf8d0f0f92c9fa03f9
Alexey Kosyuk <reddiamond821@gmail.com>
2019-05-17 18:17:14 +0300

    Add Contact and Language data transfer objects.

---
+++ Source/OnlineStore.Model/DTO/ContactDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Model.DTO
+{
+    public class ContactDTO
+    {
+        public string Location { get; set; }
+
+        public string MobilePhone { get; set; }
+
+        public string Email { get; set; }
+
+        public string WorkTime { get; set; }
+
+        public string Registration { get; set; }
+    }
+}
---
+++ Source/OnlineStore.Model/DTO/LanguageDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Model.DTO
+{
+    public class LanguageDTO
+    {
+        public string LanguageId { get; set; }
+
+        public string LanguageCode { get; set; }
+
+        public string LanguageName { get; set; }
+
+        public string ImageFilename { get; set; }
+    }
+}
