f2a78d8a07d75f7e0be2a913583803cb4cfb553c
Alexey Kosyuk <reddiamond821@gmail.com>
2019-05-28 00:02:30 +0300

    Add product controller and views.

---
+++ Source/OnlineStore.Website/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using OnlineStore.Logic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineStore.Website.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
