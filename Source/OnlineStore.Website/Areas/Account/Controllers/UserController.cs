a6ebd40602d73ae61dd60676da328adaf53b3e6c
Alexey Kosyuk <reddiamond821@gmail.com>
2019-06-02 01:05:00 +0300

    Add user controller.

---
+++ Source/OnlineStore.Website/Areas/Account/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineStore.Website.Areas.Account.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
